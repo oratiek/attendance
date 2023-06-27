c4db4cb8368a2984bf7685b90859715e5db4c938
oratiek <oratiek522@gmail.com>
2023-06-27 20:48:15 +0900

    ...

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import sys
 import os
 import time
 from datetime import datetime
-from gtts import gTTS
 import tkinter as tk
+import logging
+
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s:%(name)s - %(message)s", filename="/home/keitaro/Desktop/attendance/test.log")
 
 class Attendance():
     def __init__(self):
@@ -18,6 +20,7 @@ class Attendance():
                 pass # try again
 
         print("found reader device")
+        logging.info("found reader device")
         self.logs = []
         # read log.csv and add todays logs
         today = datetime.now().date()
@@ -30,28 +33,20 @@ class Attendance():
                 timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S").date()
                 if timestamp == today:
                     self.logs.append(row[1])
+        logging.info("attendance log csv file create")
 
     def unmount(self):
         path = "/media/keitaro/attendance"
         os.system("sudo umount {}".format(path))
         os.system("sudo shutdown now")
+        logging.info("removal device unmounted")
          
-    def student_number_to_mp3(self, student_number):
-        # ask for data
-        table = {}
-        try:
-            name = table[student_number]
-        except KeyError:
-            name = ""
-        speech_text = "good morning {}".format(name)
-        engine = gTTS(text=speech_text, lang="en", slow=False)
-        engine.save("greeting.mp3")
-
     # with other card, dump data will be different and occur errors and die
     # need to check if scanned card is correct one
     def main(self):
         #f = open(self.log_file_path, "a") # even if exception occurs, data will be saved
         #writer = csv.writer(f)
+        logging.info("main method started")
         while True:
             try:
                 tag = self.clf.connect(rdwr={'on-connect': lambda tag: False})
@@ -60,11 +55,11 @@ class Attendance():
                 student_number = target.split("002062")[0]
 
                 # close attendance
-                """
                 if student_number == "72047937":
                     print("close ID found")
+                    logging.info("close ID found")
+                    os.system("mpg123 /home/keitaro/Desktop/attendance/ok.mp3 > /dev/null")
                     break
-                """
 
                 if not student_number in self.logs:
                     timestamp = datetime.now()
@@ -76,6 +71,7 @@ class Attendance():
                     #self.student_number_to_mp3(student_number)
                     self.logs.append(student_number)
                     print(student_number)
+                    logging.info("student number {} found".format(student_number))
                     os.system("mpg123 /home/keitaro/Desktop/attendance/ok.mp3 > /dev/null")
                 else:
                     print("already registered")
@@ -84,6 +80,7 @@ class Attendance():
                 print("error")
 
 if __name__ == "__main__":
+
     media_path = "/media/keitaro/attendance"
     print("wait for removal media...")
     while True:
@@ -91,6 +88,7 @@ if __name__ == "__main__":
         if media_check:
             break
     print("media found")
+    logging.info("media found")
     os.system("mpg123 /home/keitaro/Desktop/attendance/ok.mp3")
         
     attendance = Attendance()
