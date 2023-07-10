f73e2d85a1c1eda7f406f7667c8d70b75ec6036c
oratiek <oratiek522@gmail.com>
2023-07-10 22:46:36 +0900

    working on raspi3

--- main.py
+++ main.py
@@ -1,95 +1,119 @@
-import nfc
 import csv
 import sys
 import os
 import time
+import logging
+import RPi.GPIO as GPIO
 from datetime import datetime
-import tkinter as tk
+import nfc
 import logging
 
-logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s:%(name)s - %(message)s", filename="/home/keitaro/Desktop/attendance/test.log")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s:%(name)s - %(message)s", filename="/home/keitaro/Desktop/attendance/test_log.log")
+
 
 class Attendance():
     def __init__(self):
+        # GPIO settings
+        self.ready_state_pin = 11
+        self.attend_state_pin = 13
+        self.buzzer = 15
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.ready_state_pin, GPIO.OUT)
+        GPIO.setup(self.attend_state_pin, GPIO.OUT)
+        GPIO.setup(self.buzzer, GPIO.OUT)
+
+        self.admins = ["72047937"]
+        
+        today = datetime.now().date()
+        self.log_file_path = "/home/keitaro/Desktop/attendance/log/{}.csv".format(today)
+        if not os.path.exists(self.log_file_path):
+            os.system(self.log_file_path)
+        self.log = []
+    
+    def ready(self):
+        GPIO.output(self.buzzer, 1)
+        time.sleep(0.1)
+        GPIO.output(self.buzzer, 0)
+        GPIO.output(self.ready_state_pin, 1)
+
+    def attend(self):
+        GPIO.output(self.attend_state_pin, 1)
+        GPIO.output(self.buzzer, 1)
+        time.sleep(0.1)
+        GPIO.output(self.attend_state_pin, 0)
+        GPIO.output(self.buzzer, 0)
+
+    def close_system(self):
+        for i in range(3):
+            GPIO.output(self.attend_state_pin, 1)
+            GPIO.output(self.buzzer, 1)
+            time.sleep(0.1)
+            GPIO.output(self.attend_state_pin, 0)
+            GPIO.output(self.buzzer, 0)
+            time.sleep(0.05)
+        GPIO.output(self.ready_state_pin, 0)
+
+    def already_registered(self):
+        for i in range(2):
+            GPIO.output(self.attend_state_pin, 1)
+            GPIO.output(self.buzzer, 1)
+            time.sleep(0.1)
+            GPIO.output(self.attend_state_pin, 0)
+            GPIO.output(self.buzzer, 0)
+            time.sleep(0.05)
+
+    def on_connect(self, tag):
+        sc = nfc.tag.tt3.ServiceCode(68, 0x0b)
+        bc = nfc.tag.tt3.BlockCode(1, service=0)
+        data_num = tag.read_without_encryption([sc], [bc])
+        data_num = data_num.decode("shift_jis")
+        student_id = data_num[:8]
+        print(student_id)
+        timestamp = datetime.now()
+        if student_id in self.log:
+            print("Already Registered")
+            self.already_registered()
+        else:
+            with open(self.log_file_path, "a") as f:
+                writer = csv.writer(f)
+                writer.writerow([timestamp, student_id])
+            print("attend")
+            self.attend()
+            self.log.append(student_id)
+
+        if student_id in self.admins:
+            print("exit")
+            self.close_system()
+            sys.exit(0)
+
+        return True
+
+    def battery_check(self):
+        pass
+
+    def main(self):
         while True:
             time.sleep(1)
             try:
-                self.clf = nfc.ContactlessFrontend("usb")
+                clf = nfc.ContactlessFrontend("usb")
                 break
             except OSError:
-                pass # try again
-
-        print("found reader device")
-        logging.info("found reader device")
-        self.logs = []
-        # read log.csv and add todays logs
-        today = datetime.now().date()
-        # create log file
-        self.log_file_path = "/media/keitaro/attendance/{}.csv".format(today)
-        os.system("touch {}".format(self.log_file_path))
-        with open(self.log_file_path, "r") as f:
-            for row in csv.reader(f):
-                timestamp = row[0].strip(" ")
-                timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S").date()
-                if timestamp == today:
-                    self.logs.append(row[1])
-        logging.info("attendance log csv file create")
-
-    def unmount(self):
-        path = "/media/keitaro/attendance"
-        os.system("sudo umount {}".format(path))
-        os.system("sudo shutdown now")
-        logging.info("removal device unmounted")
-         
-    # with other card, dump data will be different and occur errors and die
-    # need to check if scanned card is correct one
-    def main(self):
-        #f = open(self.log_file_path, "a") # even if exception occurs, data will be saved
-        #writer = csv.writer(f)
-        logging.info("main method started")
+                print("OSError")
+                pass
+        print("reader device found")
+        self.ready()
+        
+        # mainloop
         while True:
+            self.battery_check()
+            logging.info("mainloop started")
             try:
-                tag = self.clf.connect(rdwr={'on-connect': lambda tag: False})
-                target = tag.dump()[33]
-                target = target.split("|")[1]
-                student_number = target.split("002062")[0]
-
-                # close attendance
-                if student_number == "72047937":
-                    print("close ID found")
-                    logging.info("close ID found")
-                    os.system("mpg123 /home/keitaro/Desktop/attendance/ok.mp3 > /dev/null")
-                    break
-
-                if not student_number in self.logs:
-                    timestamp = datetime.now()
-                    timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-                    f = open(self.log_file_path, "a")
-                    writer = csv.writer(f)
-                    writer.writerow([timestamp, student_number])
-                    f.close()
-                    #self.student_number_to_mp3(student_number)
-                    self.logs.append(student_number)
-                    print(student_number)
-                    logging.info("student number {} found".format(student_number))
-                    os.system("mpg123 /home/keitaro/Desktop/attendance/ok.mp3 > /dev/null")
-                else:
-                    print("already registered")
-                    os.system("mpg123 /home/keitaro/Desktop/attendance/already_registered.mp3 > /dev/null")
+                logging.info("tag found")
+                tag = clf.connect(rdwr={'on-connect': self.on_connect})
             except nfc.tag.tt3.Type3TagCommandError:
                 print("error")
 
 if __name__ == "__main__":
-
-    media_path = "/media/keitaro/attendance"
-    print("wait for removal media...")
-    while True:
-        media_check = os.path.exists(media_path)
-        if media_check:
-            break
-    print("media found")
-    logging.info("media found")
-    os.system("mpg123 /home/keitaro/Desktop/attendance/ok.mp3")
-        
     attendance = Attendance()
     attendance.main()
